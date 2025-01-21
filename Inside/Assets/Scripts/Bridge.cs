6443160a59eebca2eed34f478dc07cbf5088b250
Pumasocks37 <pumasocks37@gmail.com>
2025-01-21 13:01:49 -0800

    Added Transition to next Layer

--- Inside/Assets/Scripts/Bridge.cs
+++ Inside/Assets/Scripts/Bridge.cs
@@ -7,9 +7,12 @@ public class Bridge : MonoBehaviour
     public bool open = false;
     public GameObject[] bridgeParts;
     public Collider2D[] colliders;
+    public GameObject[] enemies;
 
     private void Update()
     {
+        if (EnemiesAreNull()) open = true;
+
         //If the bridge is opened construct the bridge
         if (open)
         {
@@ -36,5 +39,18 @@ public class Bridge : MonoBehaviour
         {
             collider.enabled = false;
         }
+    }
+
+    //Checks to see if all enemies in an array are null
+    private bool EnemiesAreNull()
+    {
+        foreach (GameObject enemy in enemies)
+        {
+            if(enemy != null)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
