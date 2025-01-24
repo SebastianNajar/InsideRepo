caa3668334c42800bb7bbf84f2f672310c8053dd
Pumasocks37 <pumasocks37@gmail.com>
2025-01-23 18:07:44 -0800

    Added Binary Layer, Started on Final Layer

--- Inside/Assets/Scripts/CameraController.cs
+++ Inside/Assets/Scripts/CameraController.cs
@@ -5,6 +5,8 @@ public class CameraController : MonoBehaviour
     public Transform target;
     public int moveSpeed;
     public bool canMove = false;
+    public bool switchToPlayer = false;
+    public bool followPlayer = false;
 
     private void Update()
     {
@@ -18,11 +20,33 @@ public class CameraController : MonoBehaviour
                 canMove = false;
             }
         }
+        if (switchToPlayer)
+        {
+            target = GameObject.Find("Player").transform;
+            transform.position = Vector3.Lerp(transform.position, new Vector3(0, target.position.y, -10), moveSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, target.position) < 0.1f)
+            {
+                switchToPlayer = false;
+                followPlayer = true;
+            }
+        }
+        if (followPlayer)
+        {
+            transform.position = new Vector3(0, target.position.y, -10);
+        }
     }
 
     public void MoveCamera(Transform newTarget)
     {
+        switchToPlayer = false;
+        followPlayer = false;
         target = newTarget;
         canMove = true;
+    }
+
+    public void SwitchToPlayer()
+    {
+        switchToPlayer = true;
     }
 }
